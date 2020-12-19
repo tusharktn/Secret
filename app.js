37abc2c5ebee5379c3f89e586d58800d5649dd60
tusharktn <tusharkhetan78@gmail.comgit config --global user.name tushargit config --global user.email tusharkhetan78@gmail.com>
2020-12-19 21:24:07 +0530

    third commit

--- app.js
+++ app.js
@@ -202,6 +202,12 @@ app.post("/login",function(req,res){
   })
 })
 
-app.listen(3000,function(req,res){
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port,function(req,res){
   console.log("Server is working");
 })
